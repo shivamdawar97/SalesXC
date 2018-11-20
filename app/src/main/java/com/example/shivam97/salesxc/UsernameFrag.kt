ec558f442f7a1f7fd3d2747f276bc3f89046fbf5
shivamdawar97 <shivam97.dawar@gmail.com>
2018-11-20 22:17:51 +0530

    20Nov

--- app/src/main/java/com/example/shivam97/salesxc/UsernameFrag.kt
+++ app/src/main/java/com/example/shivam97/salesxc/UsernameFrag.kt
@@ -1,20 +1,29 @@
 package com.example.shivam97.salesxc
 
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-import com.example.shivam97.salesxc.R
+import com.example.shivam97.sales_xc.R
 
-class DetailsFrag : Fragment() {
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class usernamFrag : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.f_details, container, false)
+        return inflater.inflate(R.layout.f_usernam, container, false)
     }
 
 
